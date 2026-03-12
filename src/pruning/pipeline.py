afeda1cdd2b45149923a6975a38302c3bcd8199c
Josue Fleitas <josue.fle.sanc@gmail.com>
2026-03-12 17:44:14 +0100

    Adds Logging

--- src/pruning/pipeline.py
+++ src/pruning/pipeline.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from pruning.utilities.logger import setup_logger
+from pruning.utilities.metrics_logger import MetricsLogger
 from pruning.stages.stage1 import CausalPruningStage1
 # from pruning.stages.stage2 import CausalPruningStage2
 # from pruning.stages.stage3 import CausalPruningStage3
@@ -12,6 +14,12 @@ class CausalPruningPipeline:
     
     def __init__(self, config):
         self.config = config
+        
+        self.logger = setup_logger(config.full_output_dir)
+        self.metrics = MetricsLogger(config.full_output_dir)
+        
+        self.logger.info("Pipeline initialized")
+        
         self.stages = []
         self._initialize_stages()
     
@@ -28,29 +36,29 @@ class CausalPruningPipeline:
         
             stage_class = STAGE_REGISTRY[stage_name]
             
-            stage = stage_class(config=self.config, stage_idx=stage_idx)
+            stage = stage_class(
+                config=self.config,
+                stage_idx=stage_idx,
+                logger=self.logger,
+                metrics_logger=self.metrics
+            )
             self.stages.append(stage)
 
         if len(self.stages) == 0:
             raise ValueError("No pruning stages enabled in config.")
 
-        #TODO: change to logging
-        print(f"{len(self.stages)} pruning stages initialized.")
+        self.logger.info(f"{len(self.stages)} pruning stages initialized.\n")
     
     def run(self):
         """
         Runs the entire pruning pipeline.
         """
         
-        #TODO: Logging
-        
         for stage_idx, stage in enumerate(self.stages):
-            print()
-            print("=" * 60)
-            print(f"Running Stage {stage_idx + 1}: {stage.__class__.__name__}")
-            print("=" * 60)
-            print()
+            self.logger.info("=" * 60)
+            self.logger.info(f"Running Stage {stage_idx + 1}: {stage.__class__.__name__}")
+            self.logger.info("=" * 59 + "=\n")
             
             stage.run()
-    
-        #TODO: Logging
+        
+        self.logger.info("Causal Pruning Complete")
